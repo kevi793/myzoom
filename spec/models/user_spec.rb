c2ba40811f36167cdb2b997bc03b4072fbd509d4
vivek-zoomcar <vivek.pandey@zoomcar.com>
2015-07-14 12:28:20 +0530

    rspes example changed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it "should not give error if name exists" do
     user = FactoryGirl.create(:user)
-    user.name.should be_valid
+    user.should be_valid
   end
-  
+
 end
