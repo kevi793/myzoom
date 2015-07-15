7e59afe2e7b9e60573cac78656235efa15393ec6
vivek-zoomcar <vivek.pandey@zoomcar.com>
2015-07-15 13:14:26 +0530

    Syntax error changes

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,6 +2,6 @@ class City < ActiveRecord::Base
   has_many :locations, dependent: :destroy
 
   validates :name, length: {in: 2..25}, format: {with: /\A[a-zA-Z]+\z/,
-    message: "only allows letters"}, presence: true,
+    message: "only allows letters"}, presence: true
 
 end
