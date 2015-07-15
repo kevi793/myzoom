048a9ade644fea4363f9a8229df4a5b977f1dd75
vivek-zoomcar <vivek.pandey@zoomcar.com>
2015-07-15 15:16:27 +0530

    syntax changes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,12 +1,14 @@
 class Location < ActiveRecord::Base
   belongs_to :city
+
   validates :name, length: {in: 2..25}, format: {with: /\A[a-zA-Z]+\z/,
     message: "only allows letters"}, presence: true
-  validates :address, length {in: 2..100}, presence: true
+  validates :address, length: {in: 2..100}, presence: true
   validates :latitude, presence: true
   validates :longitude, presence: true
   validates_numericality_of :latitude, :longitude
   validates :map_link, url: true, presence: true
   validates :description, length: {2..100}, presence: true
   validates :location, uniqueness: {scope: [:latitude, :longitude]}
+
 end
