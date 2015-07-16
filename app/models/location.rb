916b673262a0c631f980b5d079d2db044d821a74
vivek-zoomcar <vivek.pandey@zoomcar.com>
2015-07-16 13:11:52 +0530

    model changes

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,28 +4,38 @@ class Location < ActiveRecord::Base
   validates :name,
             :length => {in: 2..25, :message => 'Must be between 2 to 25 characters'},
             :format => {with: /\A[a-zA-Z]+\z/, message: "only allows letters"},
-            :presence => {:message => 'location cannot be blak'}
+            :presence => true
 
   validates :address,
             :length => {in: 2..100, :message => 'Must be between 2 to 100 characters'},
-            :presence => {:message => 'address cannot be blak'}
+            :presence => true
 
-  validates :latitude
-            :presence => {:message => 'latitiude cannot be blank'}
-            :numericality => {only_float: true, :message => 'latitude should be float'}
+  validates :latitude,
+            :presence => {:message => 'latitiude cannot be blank'},
+            :numericality => {:message => 'latitude can be only float', only_float: true}
 
-  validates :longitude
-            :presence => {:message => 'longitude cannot be blank'}
-            :numericality => {only_float: true, :message => 'longitude should be float'}
+  validates :longitude,
+            :presence => {:message => 'longitude cannot be blank'},
+            :numericality => {:message => 'longitude can be only float', only_float: true}
 
   validates :map_link,
             :url => {:message => 'url is not correct'},
-            :presence => {:message => 'map link cannot be blank'}
-            
+            :presence => {:message => 'map link cannot be blank'},
+            :uniqueness => {:message => 'map_link already exists'}
+
   validates :description,
-            :length => {in: 2..100, :message => 'description should be between 2-100 characters'},
+            :length => {in: 2..100, :message => 'description should  be between 2-100 characters'},
             :presence => {:message => 'description cannot be blank'}
 
-  validates :location, uniqueness: {scope: [:latitude, :longitude]}
+  after_save :validate_model
+
+  def validate_model
+    epsilon = 0.00001
+    locations = Location.where(["(latitude >= ? and latitude <= ?) and (longitude >= ? and longitude <= ?)",self.latitude-epsilon,self.latitude+epsilon,self.longitude-epsilon,self.longitude+epsilon])
+    if locations.count > 1
+      self.destroy
+      errors[:base] << 'Location with same latitude and longitude already exists'
+    end
+  end
 
 end
