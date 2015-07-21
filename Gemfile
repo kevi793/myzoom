b1a0d2abc2c29ad8fa0b5a4aa3786c0611217b79
vivek-zoomcar <vivek.pandey@zoomcar.com>
2015-07-21 18:15:32 +0530

    using aasm gem

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem "activerecord-tableless"
 
 gem "aasm"
 
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
 
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,5 +4,19 @@ class Booking < ActiveRecord::Base
   belongs_to :car_group
   belongs_to :location
 
+  include AASM
 
+  enum booking_status: {
+    initiated: 4,
+    paid: 2
+  }
+  aasm :column => :booking_status, :enum => true do
+      state :initiated, initial: true
+      state :paid
+      event :make_payment do
+        transitions :from => :initiated , :to => :paid
+      end
+  end
+
+  
 end
