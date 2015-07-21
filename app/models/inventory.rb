3a8c9763e957f0ecb570e73c6ea232368364fdc8
vivek-zoomcar <vivek.pandey@zoomcar.com>
2015-07-21 13:38:27 +0530

    inventory populate

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,27 @@
 class Inventory < ActiveRecord::Base
   belongs_to :car_group
   belongs_to :location
+
+  def self.createInventory
+    carmovements = Carmovement.all
+     carmovements.each do |e|
+      start_time = e.start_time
+      end_time = e.end_time
+      car = Car.find(e.car_id)
+      car_group_id = car.car_group_id
+      while start_time < end_time
+        inventory = Inventory.where(["location_id = ? and car_group_id = ? and start_time = ? and end_time = ?",
+          e.location_id, car_group_id, start_time, start_time + 15.minutes])
+        if inventory.blank?
+          Inventory.create(:car_group_id => car_group_id, :start_time => start_time, :end_time => start_time+15.minutes,
+          :location_id => e.location_id, :number_of_cars => 1)
+        else
+          inventory[0].increment!(:number_of_cars)
+        end
+        start_time += 15.minutes
+      end
+    end
+  end
+
+
 end
